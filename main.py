5ac0a48fde07c23900cf8306dfecbbd2607a9921
Constantin Wiemer <constantin.wiemer@allianz.de>
2026-02-26 12:44:29 +0100

    Added a function to create a TGT

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ import asyncio
 import sys
 
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 
-from impacket.krb5.asn1 import AS_REQ, KRB_ERROR, PrincipalName, seq_set
-from impacket.krb5.constants import ApplicationTagNumbers, ErrorCodes, PrincipalNameType
-from impacket.krb5.crypto import Key, _get_enctype_profile, get_random_bytes
-from impacket.krb5.types import Principal
+from impacket.krb5.asn1 import (
+    AS_REQ,
+    EncTicketPart,
+    KRB_ERROR,
+    PrincipalName,
+    Ticket,
+    seq_set,
+)
+from impacket.krb5.constants import ApplicationTagNumbers, ErrorCodes, PrincipalNameType, TicketFlags
+from impacket.krb5.crypto import Key, _get_enctype_profile, encrypt, get_random_bytes
+from impacket.krb5.types import KerberosTime, Principal
 from loguru import logger
 from pyasn1.codec.der import decoder, encoder
 
@@ -18,12 +25,14 @@ from pyasn1.codec.der import decoder, encoder
 EXIT_OK = 0
 EXIT_ERROR = 1
 
+KRB_VERSION = 5
 KRB_REALM = "CWTEST.LOCAL"
 KRB_SNAME = "krbtgt"
 KRB_KNOWN_PRINCIPALS = {
     "consti@CWTEST.LOCAL": "consti123",
     "krbtgt@CWTEST.LOCAL": "krbtgt123",
 }
+KRB_DEFAULT_TICKET_VALIDITY_TIME_HOURS = 10
 
 
 class KrbError:
@@ -33,10 +42,10 @@ class KrbError:
 
     def to_asn1(self) -> bytes:
         msg = KRB_ERROR()
-        msg["pvno"] = 5
+        msg["pvno"] = KRB_VERSION
         msg["msg-type"] = ApplicationTagNumbers.KRB_ERROR.value
         now = datetime.now(timezone.utc)
-        msg["stime"] = now.strftime("%Y%m%d%H%M%SZ")
+        msg["stime"] = KerberosTime.to_asn1(now)
         msg["susec"] = now.microsecond
         msg["realm"] = KRB_REALM
         msg["error-code"] = self.code.value
@@ -81,7 +90,7 @@ class KerberosServer:
                 principal_passwd=KRB_KNOWN_PRINCIPALS[cpn],
             )
             logger.debug(
-                f"Created key {up_key.contents} for principal '{cpn}' with encryption type {enctype_to_use} "
+                f"Created key '{up_key.contents.hex(sep=' ')}' for principal '{cpn}' with encryption type {enctype_to_use} "
                 f"(the first one offered by the client)"
             )
         else:
@@ -97,21 +106,22 @@ class KerberosServer:
                 principal_passwd=KRB_KNOWN_PRINCIPALS[spn],
             )
             logger.debug(
-                f"Created key {sp_key.contents} for principal '{spn}' with encryption type {enctype_to_use} "
+                f"Created key '{sp_key.contents.hex(sep=' ')}' for principal '{spn}' with encryption type {enctype_to_use} "
                 f"(the first one offered by the client)"
             )
         else:
             logger.error(f"Service principal '{spn}' is not known")
             resp = KrbError(ErrorCodes.KDC_ERR_S_PRINCIPAL_UNKNOWN, f"User '{spn}' is not known")
             self.transport.sendto(resp.to_asn1(), addr)
-        # TODO: Create TGT
-        # TODO: Create AS_REP message
-
+        
         session_key = self._create_session_key(enctype_to_use)
         logger.debug(
-            f"Created session key {session_key.contents} for encryption type {enctype_to_use} "
+            f"Created session key '{session_key.contents.hex(sep=' ')}' for encryption type {enctype_to_use} "
             f"(the first one offered by the client)"
         )
+        tgt = self._create_tgt(cname, realm, session_key, sp_key)
+        logger.debug(f"Created TGT {tgt}")
+        # TODO: Create AS_REP message
 
 
     def _create_session_key(self, enctype: int) -> Key:
@@ -130,6 +140,71 @@ class KerberosServer:
         enctype_profile = _get_enctype_profile(enctype)
         salt = realm + str(principal_name["name-string"][0])
         return enctype_profile.string_to_key(principal_passwd, salt, params=None)
+
+
+    def _create_tgt(
+        self,
+        client_principal_name: PrincipalName,
+        client_realm: str,
+        session_key: Key,
+        service_key: Key,
+        flags: list[TicketFlags] = [],
+        duration: timedelta = timedelta(hours=KRB_DEFAULT_TICKET_VALIDITY_TIME_HOURS),
+    ) -> bytes:
+        """
+        Create a ticket-granting ticket (TGT)
+        """
+        enc_ticket_part = EncTicketPart()
+
+        flags_value = sum(1 << flag.value for flag in flags)
+        enc_ticket_part["flags"] = bin(flags_value)[2:].zfill(32)
+
+        key = seq_set(enc_ticket_part, "key")
+        key["keytype"] = session_key.enctype
+        key["keyvalue"] = session_key.contents
+
+        enc_ticket_part["crealm"] = client_realm
+        seq_set(
+            enc_ticket_part,
+            "cname",
+            Principal(
+                (str(client_principal_name["name-string"][0]), client_realm),
+                type=client_principal_name["name-type"],
+            ).components_to_asn1,
+        )
+
+        # Set transited encoding, empty for direct authentication
+        transited = seq_set(enc_ticket_part, "transited")
+        transited["tr-type"] = 0
+        transited["contents"] = b""
+
+        now = datetime.now(timezone.utc)
+        enc_ticket_part["authtime"] = KerberosTime.to_asn1(now)
+        enc_ticket_part["starttime"] = KerberosTime.to_asn1(now)
+        enc_ticket_part["endtime"] = KerberosTime.to_asn1(now + duration)
+
+        encoded_enc_ticket_part = encoder.encode(enc_ticket_part)
+        # Key usage 2 = ticket to be used in AS-REP / TGS-REP messages (encrypted with the service key)
+        cipher_text = encrypt(
+            key=service_key,
+            keyusage=2,
+            plaintext=encoded_enc_ticket_part,
+            confounder=b""
+        )
+
+        ticket = Ticket()
+        ticket["tkt-vno"] = KRB_VERSION
+        ticket["realm"] = KRB_REALM
+        seq_set(
+            ticket,
+            "sname",
+            Principal((KRB_SNAME, KRB_REALM), type=PrincipalNameType.NT_SRV_INST.value).components_to_asn1,
+        )
+        enc_part = seq_set(ticket, "enc-part")
+        enc_part["etype"] = service_key.enctype
+        enc_part["cipher"] = cipher_text
+
+        return encoder.encode(ticket)
 
 
 async def main() -> int:
